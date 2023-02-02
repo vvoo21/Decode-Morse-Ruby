b2357fb7d4414c4f90c6d8a1f9b2351b5b97d1d1
Amen TETTEH <tkamen@outlook.com>
2023-02-02 20:27:53 +0000

    Update main.rb file to add decode_wrd and decode methods

--- main.rb
+++ main.rb
@@ -12,3 +12,14 @@ def decode_char(char)
 
   morse_code[char]
 end
+
+def decode_word(word)
+  word.split.map { |char| decode_char(char) }.join
+end
+def decode(message)
+  message.split('   ').map { |word| decode_word(word) }.join(' ')
+end
+
+puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+
+# "A BOX FULL OF RUBIES"
